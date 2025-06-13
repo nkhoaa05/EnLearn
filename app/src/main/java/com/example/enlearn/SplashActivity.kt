06837e1ad91de3092e82c0d8f26eca4aeabfc30e
nkhoa <nkhoa3625@gmail.com>
2025-06-13 10:06:25 +0700

    splash

---
+++ app/src/main/java/com/example/enlearn/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.enlearn
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
