7d3dd23fabcb39b3c3ba238f60857b5afb81293b
nkhoa <nkhoa3625@gmail.com>
2025-06-13 19:39:26 +0700

    Login Screen

--- app/src/main/java/com/example/enlearn/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/enlearn/ui/screen/login/LoginScreen.kt
@@ -2,10 +2,10 @@ package com.example.enlearn.ui.screen.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +13,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,20 +45,11 @@ fun LoginScreen() {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(130.dp)
+                .height(120.dp)
                 .background(PrimaryColor)
                 .offset(y = (10.dp))
 
         ) {
-            Image(
-                painter = painterResource(R.drawable.back_icon),
-                contentDescription = "Back icon",
-                modifier = Modifier
-                    .size(40.dp)
-                    .align(Alignment.CenterStart)
-                    .offset(x = 20.dp)
-                    .clickable { }
-            )
             Text(
                 "Login",
                 fontSize = 24.sp,
@@ -67,7 +61,7 @@ fun LoginScreen() {
 
         }
 
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         Column(
             verticalArrangement = Arrangement.Top,
@@ -78,7 +72,7 @@ fun LoginScreen() {
                 painter = painterResource(R.drawable.icon_login),
                 contentDescription = "Login Icon",
                 modifier = Modifier
-                    .size(200.dp)
+                    .size(170.dp)
             )
 
             Text(
@@ -88,9 +82,60 @@ fun LoginScreen() {
             )
         }
 
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(30.dp))
 
-        Text("login",
-            color = Color.Blue)
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    "Đăng nhập nhanh với Google",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                LoginBtnThird(onClick = {}, logo = R.drawable.g_logo)
+                Spacer(modifier = Modifier.height(50.dp))
+            }
+
+        }
+
+
+    }
+}
+
+
+@Composable
+fun LoginBtnThird(onClick: () -> Unit, logo: Int) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFD5EDFF),
+            contentColor = Color(0xFF130160)
+        ),
+        modifier = Modifier
+            .height(80.dp)
+            .width(320.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = logo),
+                contentDescription = "icon",
+
+                )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                "Đăng nhập bằng Google",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
     }
 }
